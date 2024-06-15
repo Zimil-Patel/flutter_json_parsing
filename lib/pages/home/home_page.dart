584f53ef7c115bc91f9f6fc9f3b62bc2b9433485
Zimil-Patel <patelzimil2310@gmail.com>
2024-06-15 08:49:30 +0530

    15-06-2024

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,44 +1,49 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_json_parsing/providers/data_provider.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  @override
-  void initState() {
-    Provider.of<DataProvider>(context, listen: false).convertJsonToString();
-    Provider.of<DataProvider>(context, listen: false).convertToDataModel();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-
-    DataProvider provider = Provider.of<DataProvider>(context);
+    DataProvider providerT = Provider.of<DataProvider>(context);
+    DataProvider providerF = Provider.of<DataProvider>(context, listen: false);
 
     return Scaffold(
-
       appBar: AppBar(
         title: const Text('User Posts Data'),
         elevation: 4,
         shadowColor: Colors.grey.withOpacity(0.5),
       ),
-
-
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('${provider.dataModel!.total}',),)
-        ],
+      body: FutureBuilder<String>(
+        future: rootBundle.loadString('assets/json/json_data.json'),
+        builder: (context, snapshot) {
+          if (snapshot.data != null) {
+            Map data = jsonDecode(snapshot.data!);
+            providerF.convertToDataModel(data);
+            return Center(
+              child: ListView(
+                children: [
+                  ...List.generate(
+                    providerT.dataModel!.postList.length,
+                    (index) => ListTile(
+                      title: Text(
+                          'ID: ${providerT.dataModel!.postList[index].id}'),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const Center(child: CupertinoActivityIndicator());
+          }
+        },
       ),
-
     );
   }
 }
